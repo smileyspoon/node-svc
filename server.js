806c319c742ea038c7e26bc883bb84ef1427c843
Charles Betz <betz4871@stthomas.edu>
2020-08-29 22:15:33 +0000

    updated server.js

--- server.js
+++ server.js
@@ -20,59 +20,66 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.write(dateIPStamp({ "action":"GET" }, req.ip));
   res.status(200).end();
-  console.log('Console: Server returned success on get.')
+  console.log('Console: / Server returned success on get.')
 });
-
-app.post('/', (req, res) => {
-  let recd = req.body;
-  recd.action = "POST";
-  res.write(dateIPStamp(recd), req.ip); //
-  res.status(200).end();
-  console.log('Console: server returned success on post.')
-});
-
-// this is where I need to think hard. 
-// this code could be running on any node
-// if someone posts to /1, what to do? 
-// call node-svc-01? that could be a loop. 
-// I really don't want any conditionals
-// how about: if you post to /1, it calls all the 2s 
-// that's an OK start
-
 
 app.get('/1', (req, res) => {
-   fetch('http://localhost:3000')
-    .then(response => response.json())
-    .then(data => {
-   console.log("/1 subrequest rec'd" + JSON.stringify(data))
-  })
-
-  res.write(data);
+   let recd = fetch('http://node-svc-01:3000')
+         .then(response => response.json())
+         .then(resjson => {console.log("/1 subrequest rec'd" + JSON.stringify(resjson)); })
+  res.write(dateIPStamp(recd, req.ip));
   res.status(200).end();
   console.log('Console: /1 Server returned success on get.')
 });
 
+app.post('/', (req, res) => {
+  console.log ("Console: entered / post");
+  console.log("Console: / received " + JSON.stringify(req.body));
+  let recd = req.body;
+  recd.action = "POST";
+  let stampedRecd = dateIPStamp(recd, req.ip);
+  res.write(stampedRecd); //
+  res.status(200).end();
+  console.log('Console: / returned ' + stampedRecd);
+});
 
 app.post('/1', (req, res) => {
-  var myData = req.body;
-  console.log('Console: server got json:', myData);
-  var now = new Date();
-  myData.ip = req.ip;
-  myData.date = now;
-  console.log('Console: server updated json:', myData); 
-
-  var myData2 = JSON.stringify(myData);
-  res.write(myData2); //
+  console.log ("Console: entered /1 post");
+  let recd = req.body;
+  console.log('Console: /1 received southbound POST with ' + JSON.stringify(recd));
+  recd.action = "POST";
+  const url ='http://node-svc-01:3000';
+  console.log("Console: /1 attempting POST with " + JSON.stringify(recd));
+  // try passing res into the fetch function
+  let recd2 =   (async () => {
+        const body = recd;
+	const response = await fetch(url, {
+		method: 'post',
+		body: JSON.stringify(body),
+		headers: {'Content-Type': 'application/json'}
+	});
+	const json = await response.json();
+        recd2 = await dateIPStamp(json, req.ip);
+	console.log("Console: /1 received northbound " +recd2);
+        return recd2;
+  })();
+   console.log("Console: exited fetch with " + recd2);
+  res.write("placeholder");
+  //res.write(recd2);
   res.status(200).end();
-  console.log('Console: server returned success on post.')
-});
+  console.log('Console: /1 ending post.')
+  }
+);
 
 app.get('/2', (req, res) => {
-  res.write('Response: /2 successful get from ');
-  res.write(req.ip);
+   let recd = fetch('http://node-svc-01:3000/1')
+         .then(response => response.json())
+         .then(resjson => {console.log("/2 subrequest rec'd" + JSON.stringify(resjson)); })
+  res.write(dateIPStamp(recd, req.ip));
   res.status(200).end();
-  console.log("Console: /2 Server completed get.\n")
+  console.log('Console: /2 Server returned success on get.')
 });
+
 
 app.listen(PORT, HOST);
 
@@ -93,25 +100,22 @@ const url ='http://localhost:3000';
 const headers = {
   "Content-Type": "application/json"
 };
-const data = JSON.stringify({
+const postData = JSON.stringify({
   "firstName": "myFirstName",
   "lastName": "myLastName"
 });
 
-fetch(url, { method: 'POST', headers: headers, body: data})
+fetch(url, { method: 'POST', headers: headers, body: postData})
   .then((res) => {
      return res.json()
 })
 .then((json) => {
    // Do something with the returned data.
   console.log(json);
-
 });
 
-
-
-
 function dateIPStamp(recdJSON, someIP) {
+  console.log ("DateIPStamp reached with " + JSON.stringify(recdJSON) + " " + someIP);
   recdJSON.ip = someIP;
   let now = new Date();
   recdJSON.date = now;
