432672fd3cea61658361689462d99dc3a3de5880
Charles Betz <betz4871@stthomas.edu>
2020-08-29 13:13:02 +0000

    updated server.js

--- getnew.sh
+++ getnew.sh
@@ -2,6 +2,7 @@
 
 # pull from 01
 scp -r node-user@${NODE_IP_01}:/home/node-user/node-svc-v1/server.js /home/betz4871/repos/node-svc-v1/
+scp -r node-user@${NODE_IP_01}:/home/node-user/node-svc-v1/package.json /home/betz4871/repos/node-svc-v1/
 
 #commit
 git add . -A
