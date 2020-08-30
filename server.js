36c078b3ba84e139b902b3704ba72dc3c8777860
Charles Betz <betz4871@stthomas.edu>
2020-08-30 22:39:03 +0000

    updated server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 // simple microservice. 
 
 'use strict';
-const arrNodes = [ "node-svc-01" ]
-//const arrNodes = [ "node-svc-01", "node-svc-02" ]
+//const arrNodes = [ "node-svc-01" ]
+const arrNodes = [ "node-svc-01", "node-svc-02" ]
 //const arrNodes = [ "node-svc-01", "node-svc-02" , "node-svc-03" ]
 const express = require('express');
 const fetch = require('node-fetch');
@@ -20,35 +20,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/0?', (req, res) => {     // matches either / or /0
-  (async () => {
-  const myWrite = await res.write(dateIPStamp({ "action":"GET" }, req.ip));
-  const myEnd = await res.status(200).end();
-  await console.log('Console: / Server returned success on get.');
-  })();
+  //(async () => {
+    res.write(dateIPStamp({ "action":"GET" }, req.ip));
+    res.status(200).end();
+    console.log('Console: / Server returned success on get.');
+  //y})();
 });
 
 // app.get('/:depth(\d+)', (req, res) => {   // WHY does this not work
 app.get('/:depth', (req, res) => {   // everything else but / or /0
-  console.log("/1 GET, making GET subrequest");
-  console.log ("got "+ JSON.stringify(req.params));
-  console.log ("isNumeric " + isNumeric(req.params.depth));
-  if (!isNumeric(req.params.depth)) { 
-    console.log("non-numeric path attempted");
-    res.write("error, only numeric depth path supported");
-    res.status(405).end();
-    return;
-  }
-  // what is the formula for which node to call? 
-  // given x levels and n nodes
-  // x % n  where x>n, else n
-  let intCurrLevel = parseInt(req.params.depth);
-  let numNodes = arrNodes.length; // to be derived from arrNodes
-  let nextLevel = intCurrLevel - 1;
-  console.log ("nextLevel " + nextLevel + " numNodes " + numNodes);  
-  let nextNode = nextLevel >= numNodes ? nextLevel % numNodes : nextLevel;
-  let strURL = buildURL(arrNodes[nextNode], nextLevel);
-  console.log ("calling node " + strURL);
-
+  console.log("/n GET, making GET subrequest");
+  if (!boolValidateRoute(res, req.params.depth)) return;
+  let strURL = buildURL(req.params.depth);
   (async () => {
 	const response = await fetch(strURL);
 	const json = await response.json();
@@ -59,19 +42,8 @@ app.get('/:depth', (req, res) => {   // everything else but / or /0
   })();
 });
 
-app.get('/2', (req, res) => {
-  console.log("/2 GET, making GET subrequest");
-  (async () => {
-        const response = await fetch('http://node-svc-02:3000/1');
-        const json = await response.json();
-        console.log(json);
-        res.write(dateIPStamp(json, req.ip));  
-        res.status(200).end();
-        console.log('Console: /2 Server returned success on get.');
-  })();
-});
 
-app.post('/', (req, res) => {
+app.post('/0?', (req, res) => {     // matches either / or /0
   console.log ("Console: entered / post");
   console.log("Console: / received " + JSON.stringify(req.body));
   let recd = req.body;
@@ -82,14 +54,16 @@ app.post('/', (req, res) => {
   console.log('Console: / returned ' + stampedRecd);
 });
 
-app.post('/1', (req, res) => {
-  console.log ("Console: /1 POST");
+app.post('/:depth', (req, res) => {
+  console.log ("Console: /n POST");
+  if (!boolValidateRoute(res, req.params.depth)) return;
+  let strURL = buildURL(req.params.depth);
   (async () => {
-        console.log("/1 POST trying subrequest");
+        console.log("/n POST trying subrequest");
         const recd = await req.body;
         const headers = {"Content-Type": "application/json"};
         const postData = await JSON.stringify({recd});
-        const response = await fetch('http://node-svc-01:3000', { method: 'POST', headers: headers, body: postData});
+        const response = await fetch(strURL, { method: 'POST', headers: headers, body: postData});
         const json = await response.json();
         console.log(json);
         res.write(dateIPStamp(json, req.ip));  
@@ -131,6 +105,14 @@ fetch(url, { method: 'POST', headers: headers, body: postData})
   console.log(json);
 });
 
+function boolValidateRoute (res, strRoute) {
+ if (!isNumeric(strRoute)) { 
+    console.log("non-numeric path attempted");
+    res.write("error, only numeric depth path supported");
+    res.status(405).end();
+    return false; } else  { return true; }
+  }
+
 
 function dateIPStamp(recdJSON, someIP) {
   console.log ("DateIPStamp reached with " + JSON.stringify(recdJSON) + " " + someIP);
@@ -151,7 +133,22 @@ function isNumeric(strIn) {
   return (re.test(strIn));
 }
  
-function buildURL (strHostIn, intLevel) {
-   console.log  ("entered buildURL with strHostIn: " + strHostIn + " intLevel " + intLevel);
-   return("http://" + strHostIn + ":3000/" + intLevel);
+function buildURL (strLevel) {
+
+
+  // what is the formula for which node to call? 
+  // given x levels and n nodes
+  // x % n  where x>n, else n
+  
+      
+  let intCurrLevel = parseInt(strLevel);
+  let nextLevel = intCurrLevel - 1;
+  let numNodes = arrNodes.length; // to be derived from arrNodes
+  let nextNode = nextLevel >= numNodes ? nextLevel % numNodes : nextLevel;
+  let strURL = "http://"+ arrNodes[nextNode] + ":3000/" + nextLevel;
+    
+  console.log ("returning URL " + strURL);
+   return(strURL);
+
 }
+
