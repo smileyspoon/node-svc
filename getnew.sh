88ed8229e3bc0f5ade4bf616fb2dfcd449fb141d
Charles Betz <betz4871@stthomas.edu>
2020-08-30 15:50:33 +0000

    updated server.js

--- getnew.sh
+++ getnew.sh
@@ -13,10 +13,12 @@ git push origin 02a
 
 # kill and restart 01
 rsh node-user@${NODE_IP_01} "sudo pkill 'nodejs'"
+rsh node-user@${NODE_IP_01} "sudo pkill 'node'"
 rsh node-user@${NODE_IP_01} "sudo nodejs node-svc-v1/server.js &"
 
 # push to 02 and restart
 rsh node-user@${NODE_IP_02} "sudo pkill 'nodejs'"
+rsh node-user@${NODE_IP_02} "sudo pkill 'node'"
 rsh node-user@${NODE_IP_02} sudo chmod 666 /home/node-user/node-svc-v1/server.js
 scp -r ~/repos/node-svc-v1/server.js node-user@${NODE_IP_02}:/home/node-user/node-svc-v1
 rsh node-user@${NODE_IP_02} "sudo nodejs node-svc-v1/server.js &"
