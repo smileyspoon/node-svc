d1ba3fd5981435803971242a910718d694652ac8
mek88 <photomek@gmail.com>
2020-09-20 13:24:15 -0500

    correct unzip command in terraform update bash script

--- update-terraform.sh
+++ update-terraform.sh
@@ -4,7 +4,7 @@
 # only need this as long as Google Cloud Console is behind. 
 
 wget https://releases.hashicorp.com/terraform/0.13.2/terraform_0.13.2_linux_amd64.zip
-unzip https://releases.hashicorp.com/terraform/0.13.2/terraform_0.13.2_linux_amd64.zip
+unzip terraform_0.13.2_linux_amd64.zip
 mkdir -p ~/bin
 mv terraform ~/bin
 rm terraform.tfs*
