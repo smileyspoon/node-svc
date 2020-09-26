cefc6e8f139cf903b943c29b80c21bd392379325
mek88 <photomek@gmail.com>
2020-09-25 19:21:18 -0500

    add git install to config script

--- config.sh
+++ config.sh
@@ -3,6 +3,7 @@ set -e  # exit immediately if anything returns non-zero. See https://www.javatpo
 
 echo "  ----- install node, npm, git -----  "
 apt-get update
+apt-get install git
 apt-get install -y nodejs npm 
 
 # we are also going to install python for using ansible later
